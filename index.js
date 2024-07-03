3cffdc0c10284a7527ddf9df592a4c38714c5ca9
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-07-03 19:21:21 +0300

    modifies elements in DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//no longer has DOM dode 'main#main'
+const element = document.querySelector('#main');
+element.remove();
+//has a 'newHeader' variable that points to an <h1> node
+const newHeader = document.createElement('h1');
+//the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.setAttribute("id","victory");
+//the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
+newHeader.textContent = "ALVIN-KIMUTAI is the champion";
